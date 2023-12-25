05ae96034f3c08469d42d89fd5ecf19a434aeda2
Vitaliy Tarassov <apxustudio@gmail.com>
2023-12-25 12:27:50 +0600

    feat: hard tasks

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  const {
+    times = 1,
+    separator = '+',
+    addition = '',
+    additiontimes = 1,
+    additionSeparator = '|'
+  } = options;
+
+  const repeatedAdd = repeatString(addition, additiontimes, additionSeparator);
+  const repeatedStr = repeatString(str + repeatedAdd, times, separator);
+
+  return repeatedStr;
 }
 
 module.exports = {
--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -13,10 +13,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  calculateDepth(arr) {
+    // if (!Array.isArray(arr)) {
+    //   return 0;
+    // }
+
+    let res = 1;
+
+    for (const item of arr) {
+      if (Array.isArray(item)) {
+        const depth = this.calculateDepth(item) + 1;
+        res = Math.max(res, depth);
+      }
+    }
+
+    return res;
   }
+  
 }
 
 module.exports = {
