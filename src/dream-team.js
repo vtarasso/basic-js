10889143a551f062a325d3e943f36fd6fe23f31a
Vitaliy Tarassov <apxustudio@gmail.com>
2023-12-25 11:51:41 +0600

    feat: task dream-team done

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam( members ) {
+  if (!Array.isArray(members)) {
+    return false;
+  }
+  // сначала убираем у массива с помощью три лишние пробелы,
+  //  вытаскиваем первый символ, увеличиваем его и добавляем в массив
+  const dreamTeamName = members.reduce((name, member) => {
+    if (typeof member === 'string') {
+      name.push(member.trim()[0].toUpperCase());
+    }
+    return name;
+  }, []).sort().join('');
+
+  return dreamTeamName;
 }
 
 module.exports = {
